b7c20504b24a5c2aa3ddd2d19d20ba042106914a
Gorokhov Dmitriy <gorokhovdmit@gmail.com>
2015-12-01 22:27:08 +0300

    Add functions to track_features.py

--- code/track_features.py
+++ code/track_features.py
@@ -2,9 +2,9 @@ from features import mfcc
 from features import logfbank
 import scipy.io.wavfile as wav
 
-def get_covariance_matrix(fbank_feat):
-    num_segments = len(fbank_feat)
-    num_features = len(fbank_feat[0])
+def _get_covariance_matrix(mfcc_feat):
+    num_segments = len(mfcc_feat)
+    num_features = len(mfcc_feat[0])
 
     cov_mat = []
     for i in range(num_features):
@@ -14,9 +14,9 @@ def get_covariance_matrix(fbank_feat):
             mean_1 = 0
             mean_2 = 0
             for k in range(num_segments):
-            	mean_1 += fbank_feat[k][i]
-            	mean_2 += fbank_feat[k][j]
-                value += fbank_feat[k][i] * fbank_feat[k][j]
+                mean_1 += mfcc_feat[k][i]
+                mean_2 += mfcc_feat[k][j]
+                value += mfcc_feat[k][i] * mfcc_feat[k][j]
             mean_1 /= num_segments
             mean_2 /= num_segments
             value /= num_segments
@@ -24,27 +24,60 @@ def get_covariance_matrix(fbank_feat):
         cov_mat.append(tmp_mat)
     return cov_mat
 
-def get_features_mean(fbank_feat):
-	num_segments = len(fbank_feat)
-	num_features = len(fbank_feat[0])
+def _get_features_mean(mfcc_feat):
+    num_segments = len(mfcc_feat)
+    num_features = len(mfcc_feat[0])
 
-	mean = [0 for i in range(num_features)]
-	for i in range(num_segments):
-		mean += fbank_feat[i]
-	for i in range(num_features):
-		mean[i] /= num_segments
+    mean = [0 for i in range(num_features)]
+    for i in range(num_segments):
+        mean += mfcc_feat[i]
+    for i in range(num_features):
+        mean[i] /= num_segments
 
-	return mean
+    return mean
 
 def get_track_features(track_name):
-	(rate,sig) = wav.read(track_name)
-	mfcc_feat = mfcc(sig,rate)
-	fbank_feat = logfbank(sig,rate)
+    (rate,sig) = wav.read(track_name)
+    mfcc_feat = mfcc(sig,rate)
+    fbank_feat = logfbank(sig,rate)
 
-	num_segments = len(fbank_feat)
-	num_features = len(fbank_feat[0])
+    num_segments = len(mfcc_feat)
+    num_features = len(mfcc_feat[0])
 
-	features_mean = get_features_mean(fbank_feat)
-	cov_mat = get_covariance_matrix(fbank_feat)
+    features_mean = _get_features_mean(mfcc_feat)
+    cov_mat = _get_covariance_matrix(mfcc_feat)
 
-	return (features_mean, cov_mat)
+    return (features_mean, cov_mat)
+
+def get_features(dir_path, *genre_names): 
+    features = []   
+    for genre_name in genre_names:
+        genre_dir_path = dir_path + "/" + genre_name +"/"
+        track_list = os.listdir(genre_dir_path)
+
+        for index in range(len(track_list)):
+            track_list[index] = genre_dir_path + track_list[index]
+
+        for track in track_list:
+            (mean, cov_mat) = get_track_features(track)
+            features.append((mean, cov_mat, genre_name))
+
+    return features
+
+def get_genre_ID(genre_name):
+    track_ID = ""
+    for ch in genre_name:
+        track_ID += str(ord(ch))
+
+    return int(track_ID)
+
+def get_genre_name(genre_ID):
+    genre_ID_list = list(str(genre_ID))
+    genre_name = ""
+    i = 0
+    while (i < len(genre_ID_list)):
+        genre_name_ch = genre_ID_list[i] + genre_ID_list[i + 1] + genre_ID_list[i + 2] 
+        genre_name += chr(int(genre_name_ch))
+        i += 3
+        
+    return genre_name
